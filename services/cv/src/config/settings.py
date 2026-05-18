0bb897e5b92e08e418824a46b5578dce10e7b8de
nikol <nikolaysem2004@gmail.com>
2026-05-19 00:51:35 +0300

    добавил необходимые для подключения к бд переменные, обновил requirements.txt

--- services/cv/src/config/settings.py
+++ services/cv/src/config/settings.py
@@ -2,9 +2,21 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # Broker settings
     rabbitmq_host: str
     rabbitmq_port: int = 5672
     rabbitmq_user: str
     rabbitmq_password: str
+
+    # DB settings
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+
+    # App settings
+    camera_ids: list[int]
+    model_path: str = "../yolo11n.pt"
 
     model_config = {"env_file": ".env"}
